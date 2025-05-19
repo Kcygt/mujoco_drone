0c1e6ed4a1208e2ba6a5fef1b3701eab073e3f91
xinsongyan <xinsongyan@users.noreply.github.com>
2025-05-19 16:26:20 +0100

    load robot in mujoco sim done

--- load_robot.py
+++ load_robot.py
@@ -1,17 +1,33 @@
 import os
 import mujoco
 import mujoco.viewer 
-
-xml_path = os.path.join('skydio_x2', 'scene.xml')
-print(f"xml_path: {xml_path} ({type(xml_path)})")
-
-
-model = mujoco.MjModel.from_xml_path(xml_path)
-data = mujoco.MjData(model)
+import numpy as np
+import transformations as tf 
+import time
 
 
 
 
-with mujoco.viewer.launch(model, data) as viewer:
+
+class Drone: 
+    def __init__(self):
+        self.xml_path = os.path.join('skydio_x2', 'scene.xml')
+        print(f"xml_path: {self.xml_path} ")
+        
+        self.m = mujoco.MjModel.from_xml_path(self.xml_path)
+        self.d = mujoco.MjData(self.m)
+
+drone = Drone()
+
+with mujoco.viewer.launch_passive(drone.m, drone.d) as viewer:
     while viewer.is_running():
-        mujoco.mj_step(model, data)
+        # print time
+        print(f"Time: {drone.d.time:.2f}s")
+        
+        mujoco.mj_step(drone.m, drone.d)
+        viewer.sync()
+
+
+
+        time.sleep(drone.m.opt.timestep)  # Sleep to limit the update rate
+
